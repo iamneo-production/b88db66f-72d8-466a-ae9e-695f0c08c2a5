95536c44cbc9317a15dae56b94f84983f4ea1b90
VijayVPatil <vijaypatil2532000@gmail.com>
2023-01-25 11:37:54 +0000

    Commit to current Branch

---
+++ springapp/src/main/java/model/Booking.java
@@ -0,0 +1,37 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Booking {
+	@Id
+	private int id;
+	private String name;
+	private String mname;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMname() {
+		return mname;
+	}
+
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+
+}
